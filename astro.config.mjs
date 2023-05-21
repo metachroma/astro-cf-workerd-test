1406d15f5411e00aaca74d29b73dcd6b71b413e2
petertjmills <56031935+petertjmills@users.noreply.github.com>
2023-05-21 16:27:56 +0100

    remove astro-workerd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
-import cloudflare from 'astro-workerd'
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	output: 'server',
-	integrations: [mdx()],
-	adapter: cloudflare()
+  site: 'https://example.com',
+  output: 'server',
+  integrations: [mdx()],
+  adapter: cloudflare()
 });
